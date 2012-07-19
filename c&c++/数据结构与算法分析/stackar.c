0e1e9bac80e113cfb6dabd74f3a7277b13da48ad
JerryXGH <jerryxgh@gmail.com>
2012-07-19 14:38:09 +0800

    Achieve stack in link table.

--- c&c++/数据结构与算法分析/stackar.c
+++ c&c++/数据结构与算法分析/stackar.c
@@ -1,4 +1,4 @@
-* @(#)stackar.c
+/* @(#)stackar.c
  */
 
 #include "stackar.h"
@@ -20,4 +20,54 @@ int IsEmpty(Stack S) {
 
 int IsFull(Stack S) {
     return S->Capacity == S->TopOfStack + 1;
+}
+
+Stack CreateStack( int MaxElements ) {
+    if (MaxElements < MinStackSize)
+        Error("Stack size is too small.");
+
+    Stack S = malloc(sizeof(struct StackRecord));
+    if (S == NULL)
+        FatalError("Out of sapce!");
+    
+    S->Array = malloc(MaxElements * sizeof(ElementType)); 
+    if (S->Array == NULL)
+        FatalError("Out of space!");
+    S->Capacity = MaxElements;
+    MakeEmpty(S);
+    return S;
+}
+
+void MakeEmpty(Stack S) {
+    S->TopOfStack = EmptyTOS;
+}
+
+void Push( ElementType X, Stack S ) {
+    if (IsFull(S))
+        FatalError("Stack is full!");
+    else
+        S->Array[++S->TopOfStack] = X;
+}
+
+ElementType Top( Stack S ) {
+    if (IsEmpty(S)) {
+        Error("Stack is empty!");
+        return 0;
+    } else
+        return S->Array[S->TopOfStack];
+}
+
+void Pop( Stack S ) {
+    if (IsEmpty(S))
+        Error("Stack is empty!");
+    else
+        --S->TopOfStack;
+}
+
+ElementType TopAndPop( Stack S ) {
+    if (IsEmpty(S)) {
+        Error("Stack is empty!");
+        return 0;
+    } else
+        return(S->Array[S->TopOfStack--]);
 }
